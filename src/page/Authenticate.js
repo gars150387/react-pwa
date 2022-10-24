42b02934f4c809f66b08e093ad466fdd0cd75bf1
gars150387 <gar.santeliz@gmail.com>
2022-10-24 19:46:18 -0400

    building app to test pwa

--- src/page/Authenticate.js
+++ src/page/Authenticate.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-import { useStytch, useStytchSession } from "@stytch/stytch-react"
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useStytch, useStytchSession } from "@stytch/stytch-react";
 
 export const Authenticate = () => {
-    const client = useStytch()
-    const session = useStytchSession()
-    const navigate = useNavigate()
+  const client = useStytch();
+  const session = useStytchSession();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-if(session){
-    navigate("/account")
-} else {
-    const token = new URLSearchParams(window.location.search).get('token')
+  const dependecies = [client, session]
 
-    client.magicLink.authenticate( token, {
-        session_duration_minutes: 60
-    }).then(() => {
-        alert("Successfully authentcated!")
-        navigate(0)
-    })}
-    },[client,session])
-    
-  return (
-    <div>Loading...</div>
-  )
-}
+  useEffect(() => {
+    if (session) {
+      navigate("/account");
+    } else {
+      const token = new URLSearchParams(window.location.search).get("token");
+
+      client.magicLinks.authenticate(token, {
+          session_duration_minutes: 60,
+        })
+        .then(() => {
+          alert("Successfully authentcated!");
+        });
+    }
+  }, [dependecies]);// eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div>Loading...</div>;
+};
