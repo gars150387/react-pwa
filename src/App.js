31d49a2e8cf0462e2696694860efb433877c098a
gars150387 <gar.santeliz@gmail.com>
2022-10-24 19:16:07 -0400

    building app to test pwa

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Navbar } from "./component/Navbar"
 import { Account } from "./page/Account"
 import { Home } from "./page/Home"
 import { Login } from "./page/Login"
-import { Register } from "./page/Register"
 import {useStytchSession } from "@stytch/stytch-react"
 
 export const App = () => {
